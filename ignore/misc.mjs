2946257ee1d5a70ba097f13347441a194a9adc55
Luke Wu <lukewu28@gmail.com>
2023-12-30 02:15:36 -0600

    Optimize 2 #2

---
+++ ignore/misc.mjs
@@ -0,0 +1,15 @@
+import { SmartContract, stackInt, stackCell, stackTuple} from "ton-contract-executor";
+import { Cell, beginCell } from "ton-core";
+// import assert from "assert";
+import fs from "fs";
+
+async function main() {
+
+    let a = beginCell().storeUint(514, 14).endCell()
+    let b = beginCell().storeUint(0, 1).storeUint(1, 4).storeUint(1, 8).storeUint(0, 1).endCell();
+    console.log(a.equals(b));
+}
+main().then(() => {
+    console.log("✅ All Tests Passed")
+    process.exit(0);
+});
